70c6f502c96e629cd241583f9b7ebbc03745d366
justsayno <contact@sethreid.co.nz>
2016-03-28 14:17:35 +1300

    added actionso

--- React-DotNet.Client/src/views/Employee/EmployeeDashboardView.js
+++ React-DotNet.Client/src/views/Employee/EmployeeDashboardView.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import { EmployeeDashboard } from 'components/employees'
 
 // redux
-import { EmployeeDashboardSelector } from 'redux/modules/employees'
+import { EmployeeDashboardSelector, fetchAllEmployees } from 'redux/modules/employees'
 
 // We avoid using the `@connect` decorator on the class definition so
 // that we can export the undecorated component for testing.
@@ -15,7 +15,21 @@ export class EmployeeDashboardView extends Component {
     employees: PropTypes.arrayOf(PropTypes.shape({
       fullName: PropTypes.string.isRequired,
       role: PropTypes.string.isRequired
-    })).isRequired
+    })).isRequired,
+    hasLoaded: PropTypes.bool.isRequired,
+    didInvalidate: PropTypes.bool.isRequired,
+    fetchAllEmployees: PropTypes.func.isRequired
+  }
+
+  componentDidMount () {
+    this.fetchEmployees(this.props)
+  }
+
+  fetchEmployees = (props) => {
+    const {hasLoaded, didInvalidate} = props
+    if (!hasLoaded || didInvalidate) {
+      this.props.fetchAllEmployees()
+    }
   }
 
   render () {
@@ -27,4 +41,5 @@ export class EmployeeDashboardView extends Component {
 }
 
 export default connect((EmployeeDashboardSelector), {
+  fetchAllEmployees
 })(EmployeeDashboardView)
